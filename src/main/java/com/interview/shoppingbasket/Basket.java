1b12d025ccd7a9b4ff18a7854abd2c305f7615b8
davibrito1218 <davibrito1218@gmail.com>
2023-01-31 15:41:30 +0000

    removing unnecessary dependencies

--- src/main/java/com/interview/shoppingbasket/Basket.java
+++ src/main/java/com/interview/shoppingbasket/Basket.java
@@ -4,11 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 public class Basket {
     private List<BasketItem> items = new ArrayList<>();
